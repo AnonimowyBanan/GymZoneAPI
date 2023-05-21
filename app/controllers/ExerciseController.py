e4084cff9d2f79c8e7804e98a7fa8de4c9bfb335
AnonimowyBanan <halama.robert497@gmail.com>
2023-05-21 20:05:54 +0200

    get specify exercise

--- app/controllers/ExerciseController.py
+++ app/controllers/ExerciseController.py
@@ -8,8 +8,6 @@ def get_all_exercises():
 
     result = []
 
-    exercises = Exercise.query.all()
-
     try:
         exercises = Exercise.query.all()
 
@@ -25,6 +23,23 @@ def get_all_exercises():
 
             return make_response({'error': str(e)}, 500)
 
+@exercise.route('/get', methods=['GET'])
+def get_exercise_data():
+    
+    try:
+        exercise = Exercise.query.filter_by(id=request.form.get('exercise_ID')).first()
+
+        if exercise == None:
+            return make_response({'error': 'exercise not found'}, 204)
+        
+        result = put_exercise_data_to_json(exercise)
+
+        return make_response(jsonify(result), 200)
+    
+    except Exception as e:
+     
+        return make_response({'error': str(e)}, 500)
+
 def put_exercise_data_to_json(data):
 
     result = {
@@ -36,6 +51,7 @@ def put_exercise_data_to_json(data):
         },
         'muscle'        : {
             'id'            : data.body_part_id_muscle.id,
+            'name'          : data.body_part_id_muscle.name,
             'picture'       : data.body_part_id_muscle.picture
         }
     }
--- app/models/BodyPart.py
+++ app/models/BodyPart.py
@@ -5,4 +5,5 @@ class BodyPart(db.Model):
     __tablename__   = 'body_parts'
 
     id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name        = db.Column(db.String(48), nullable=True)
     picture     = db.Column(db.String(48), nullable=True)
