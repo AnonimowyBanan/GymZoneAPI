77e32fa9b7a4bfedb83d53dae1e91ccb26aa8b1b
AnonimowyBanan <halama.robert497@gmail.com>
2023-05-28 19:07:31 +0200

    init swagger

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from dotenv import load_dotenv
 from app.extenctions import db
 from flask_restful import Api
+from flask_swagger_ui import get_swaggerui_blueprint
 import os
 
 def start_app():
@@ -19,8 +20,13 @@ def start_app():
     db.init_app(app)
     api = Api(app)
 
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "GymZone API"})
+
     from app.controllers import user, exercise, biometric_data
 
+    app.register_blueprint(swaggerui_blueprint, url_prefix="/swagger")
     app.register_blueprint(user, url_prefix="/user")
     app.register_blueprint(exercise, url_prefix="/exercise")
     app.register_blueprint(biometric_data, url_prefix="/biometric-data")
